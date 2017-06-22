23e258909afe0c3d83df95c47f4d69a1747d19ba
amilaDulanjana <amiladulanjana@hotmail.com>
2017-06-23 00:28:54 +0530

    finished

---
+++ src/components/chart.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import _ from 'lodash'
+import { Sparklines, SparklinesLine, SparklinesReferenceLine  } from 'react-sparklines';
+
+class Chart extends Component {
+
+    average(data){
+        return _.round(_.sum(data)/data.length);
+    }
+    
+    render() {
+        return (
+            <div>
+                <Sparklines height={120} width={180} data={this.props.data}>
+                    <SparklinesLine color={this.props.color} />
+                     <SparklinesReferenceLine type="avg" />
+                </Sparklines>
+                <div>{this.average(this.props.data)}</div>
+            </div>
+        );
+    }
+}
+
+export default Chart;
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,13 @@
+import { FETCH_WEATHER } from '../actions/'
+
+export default function (state=[],action){
+
+    //console.log('Action Recieved',action);
+    switch(action.type){
+        case FETCH_WEATHER:
+            return [action.payload.data,...state];
+    }
+
+     return state;
+
+}
